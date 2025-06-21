8e71251762a6a38771c03ce2dc0d7df8b941d4b5
Florin Miron <mironflorin123@yahoo.com>
2025-06-21 18:49:00 +0100

    Create the class view for all posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-class AllPostsView(ListView):
-    pass
+class AllPostsView(TemplateView):
+    template_name = 'blog/all-posts.html'
