65bca30fb19098c57f56c67d0958a5e285bd68a9
Florin Miron <mironflorin123@yahoo.com>
2025-06-21 16:01:04 +0100

    Add path names for urls

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomepageView.as_view())
+    path('', views.HomepageView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about')
 ]
