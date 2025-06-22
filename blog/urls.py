371e6380bddf9be34871de1c101524675b4c40c3
Florin Miron <mironflorin123@yahoo.com>
2025-06-22 23:14:20 +0100

    Add media file handling in development

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AllPostsView.as_view(), name='all-posts')
+    path('', views.AllPostsView.as_view(), name='all-posts'),
+    path('<slug:slug>', views.SinglePostView.as_view(), name='single-post')
 ]
