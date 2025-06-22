4b7659a228ed8835b686e23665c14bd900597c95
Florin Miron <mironflorin123@yahoo.com>
2025-06-22 23:10:35 +0100

    Create the Author and Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from django.core.validators import MinLengthValidator
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=300)
+    date = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to='images', null=True)
+    image_alt = models.CharField(max_length=255, default='No alt text provided')
+    slug = models.SlugField(unique=True)
+    comment = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name='posts')
+
+    def __str__(self):
+        return self.title
