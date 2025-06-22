94b87b3c6df57dc184206c44dfbd344c118ff838
Florin Miron <mironflorin123@yahoo.com>
2025-06-22 23:12:39 +0100

    Register the models to the admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Author
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'date')
+    list_filter = ('title', 'date', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
