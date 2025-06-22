f553cdc9d1d187d6d4a336bdb4ae6f51582bc2ed
Florin Miron <mironflorin123@yahoo.com>
2025-06-22 23:36:02 +0100

    Create the views for all posts and individual posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import View
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
 
-class AllPostsView(TemplateView):
+class AllPostsView(ListView):
     template_name = 'blog/all-posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
+
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+
+        return render(request, 'blog/single-post.html', {
+            "post": post
+        })
