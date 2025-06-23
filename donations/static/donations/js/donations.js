22edb36905283b1161f08439cac36bea0d2826ee
Florin Miron <mironflorin123@yahoo.com>
2025-06-23 22:01:18 +0100

    feat: implement the initial amount range value

--- donations/static/donations/js/donations.js
+++ donations/static/donations/js/donations.js
@@ -1,3 +1,12 @@
-const setDonationAmount = function () {};
+const setDonationAmount = function () {
+  const amountRange = document.getElementById("amount-range");
+  const amount = document.querySelector(".amount");
+
+  function setAmount() {
+    amount.textContent = `£${amountRange.value}`;
+  }
+
+  setAmount();
+};
 
 export default setDonationAmount;
