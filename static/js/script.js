fb28ae343854928eee45590147d826c5c30d0719
Florin Miron <mironflorin123@yahoo.com>
2025-06-23 23:31:15 +0100

    Initiate the setDynamicPadding function

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
 const navbar = document.querySelector(".navbar");
 const contentWrapper = document.querySelector(".content-wrapper");
 const navLinks = document.querySelectorAll(".nav-link");
+
+const setDynamicPadding = function () {};
