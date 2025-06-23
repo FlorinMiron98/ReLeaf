ad3535ef43b1785eee6124a5cd413980cb62d629
Florin Miron <mironflorin123@yahoo.com>
2025-06-23 23:42:36 +0100

    feat: implement setDynamicPadding

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,17 @@ const navbar = document.querySelector(".navbar");
 const contentWrapper = document.querySelector(".content-wrapper");
 const navLinks = document.querySelectorAll(".nav-link");
 
-const setDynamicPadding = function () {};
+const setDynamicPadding = function () {
+  if (contentWrapper) {
+    const header = document.querySelector("header");
+    const sizes = navbar.getBoundingClientRect();
+    header.style.paddingTop = `${sizes.height}px`;
+    header.style.paddingBottom = `${sizes.height}px`;
+  }
+};
+
+["DOMContentLoaded", "resize"].forEach((e) => {
+  window.addEventListener(e, () => {
+    setDynamicPadding();
+  });
+});
