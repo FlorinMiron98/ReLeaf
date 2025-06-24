80a32c23c1ad0f1622ac1a39e525f9859b4b9f6e
Florin Miron <mironflorin123@yahoo.com>
2025-06-24 01:44:39 +0100

    Create the core scripts for implementing the count animation

---
+++ core/static/core/js/core.js
@@ -0,0 +1,7 @@
+const countriesReachedNum = document.querySelector(".countries-reached-num");
+const forestsRestoredNum = document.querySelector(".forests-restored-num");
+const projectsCompletedNum = document.querySelector(".projects-completed-num");
+
+const animateCount = function (el, target, speed = 50) {};
+
+export default animateCount;
