bcd14c696fcd21aa39c9ef2ae8a58cc796886b09
Florin Miron <mironflorin123@yahoo.com>
2025-06-24 19:42:43 +0100

    Create the functionality for logging in the user once they completed the registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from django.views.generic import TemplateView
+from .forms import CustomRegisterForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
 
 # Create your views here.
 
-class RegisterView(TemplateView):
+class RegisterView(FormView):
     template_name = 'accounts/register.html'
+    form_class = CustomRegisterForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user=user)
+        messages.success(self.request, 'Registration successful. Welcome!')
+        return super().form_valid(form)
