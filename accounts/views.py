b0e79cd34b43a8bf9b223dbdecf4cf07dbdc07b9
Florin Miron <mironflorin123@yahoo.com>
2025-06-24 06:23:57 +0100

    Create the register class view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class RegisterView(TemplateView):
+    template_name = 'accounts/register.html'
