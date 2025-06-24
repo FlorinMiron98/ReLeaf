22cfa79f00ddbf4f25695c34b114ab5bc01e70cb
Florin Miron <mironflorin123@yahoo.com>
2025-06-24 01:54:45 +0100

    feat: implement animateCount function to animate number increase

--- core/static/core/js/core.js
+++ core/static/core/js/core.js
@@ -2,6 +2,21 @@ const countriesReachedNum = document.querySelector(".countries-reached-num");
 const forestsRestoredNum = document.querySelector(".forests-restored-num");
 const projectsCompletedNum = document.querySelector(".projects-completed-num");
 
-const animateCount = function (el, target, speed = 50) {};
+const animateCount = function (el, target, speed = 50) {
+  let current = 0;
+
+  const interval = setInterval(() => {
+    current++;
+    el.textContent = current.toLocaleString();
+
+    if (current >= target) {
+      clearInterval(interval);
+    }
+  }, speed);
+};
+
+animateCount(countriesReachedNum, 78);
+animateCount(forestsRestoredNum, 90);
+animateCount(projectsCompletedNum, 60);
 
 export default animateCount;
