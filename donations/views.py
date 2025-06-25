bc312d535da0ba44d8cb4b03ebe7e5f551e45b48
Florin Miron <mironflorin123@yahoo.com>
2025-06-25 23:03:22 +0100

    Create the product description dynamically

--- donations/views.py
+++ donations/views.py
@@ -21,6 +21,20 @@ class CreateCheckoutSessionView(View):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
         amount = int(data['amount'])
+        description = ''
+
+        if amount <= 5:
+            description = "You're planting a tree and starting something big. Even the smallest seed grows into a forest—thank you!"
+        elif amount <= 10:
+            description = "A small grove begins! Your donation will plant multiple trees and help restore natural habitats."
+        elif amount <= 25:
+            description = "You're planting a mini forest. Your support is growing roots that will last for generations - thank you!"
+        elif amount <= 50:
+            description = "With this generous gift, you're planting dozens of trees and making a real impact on reforestation efforts."
+        elif amount <= 100:
+            description = "This donation goes a long way - you're helping entire ecosystems recover and thrive. Incredible work!"
+        elif amount <= 250:
+            description = "Your support is creating a true forest. This level of generosity drives major change for our planet's future."
 
         try:
             session = stripe.checkout.Session.create(
@@ -28,7 +42,9 @@ class CreateCheckoutSessionView(View):
                     'price_data': {
                         'currency': 'gbp',
                         'product_data': {
-                            'name': 'Tree of Hope'
+                            'name': 'Tree of Hope',
+                            'images': ['https://cdn.pixabay.com/photo/2022/09/20/20/16/oak-7468708_1280.jpg'],
+                            'description': description
                         },
                         'unit_amount': amount * 100,
                     },
