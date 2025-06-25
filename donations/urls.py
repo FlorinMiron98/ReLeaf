7697fc06faebbd21026a2b6839fd47899bdf55dc
Florin Miron <mironflorin123@yahoo.com>
2025-06-25 23:01:01 +0100

    Create the paths for the checkout session and the possible outcomes (success or fail)

--- donations/urls.py
+++ donations/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.DonationsView.as_view(), name='donations')
+    path('', views.DonationsView.as_view(), name='donations'),
+    path('create-checkout-session/', views.CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('donation-success/', views.DonationSuccessView.as_view(), name='donation-success'),
+    path('donation-fail/', views.DonationFailView.as_view(), name='donation-fail')
 ]
