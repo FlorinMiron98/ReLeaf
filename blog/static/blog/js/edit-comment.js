f8482bcbdcc4cbb7a79aaa19bf701ad5da322fae
Florin Miron <mironflorin123@yahoo.com>
2025-06-26 23:06:11 +0100

    Create the functionality for displaying the edit comment modal and assign the comment id in a dataset attribute

--- blog/static/blog/js/edit-comment.js
+++ blog/static/blog/js/edit-comment.js
@@ -1,1 +1,26 @@
+const comments = document.querySelectorAll(".comment");
 
+const editCommentModalEl = document.getElementById("edit-comment-modal");
+const editCommentModal = new bootstrap.Modal(editCommentModalEl);
+const editContentTextarea = document.getElementById("edit-comment");
+
+const getCSRFToken = function () {
+  return document.querySelector("[name=csrfmiddlewaretoken]").value;
+};
+
+const displayEditModal = function () {
+  editCommentModal.show();
+};
+
+comments.forEach((comment) => {
+  comment.addEventListener("click", (e) => {
+    if (e.target.classList.contains("comment__buttons--edit")) {
+      const commentId = e.target.closest("[data-comment-id]").dataset.commentId;
+      editCommentModalEl.setAttribute("data-comment-id", commentId);
+      const commentText =
+        e.target.parentElement.previousElementSibling.textContent;
+      editContentTextarea.value = commentText.trim();
+      displayEditModal();
+    }
+  });
+});
