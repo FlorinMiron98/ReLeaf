59a54c4d5bac3e8209f7cca12420d3d2f87f05f1
Florin Miron <mironflorin123@yahoo.com>
2025-06-26 17:56:36 +0100

    Modify the single post view so the users can leavo comments

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView
+from django.views.generic.edit import FormMixin 
+from django.views.generic import DetailView, ListView
+from django.urls import reverse
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -11,10 +13,35 @@ class AllPostsView(ListView):
     ordering = ['-date']
     context_object_name = 'all_posts'
 
-class SinglePostView(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug=slug)
+class SinglePostView(FormMixin, DetailView):
+    model = Post
+    template_name = 'blog/single-post.html'
+    context_object_name = 'post'
+    form_class = CommentForm
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
 
-        return render(request, 'blog/single-post.html', {
-            "post": post
-        })
+    def get_success_url(self):
+        return reverse('single-post', kwargs={'slug': self.object.slug})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        if 'form' not in context:
+            context['form'] = self.get_form()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.author = self.request.user
+        comment.save()
+        return super().form_valid(form)
