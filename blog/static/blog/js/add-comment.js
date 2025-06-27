da90bee9a7ef100e47406ab54eed71ec55f7a1ff
Florin Miron <mironflorin123@yahoo.com>
2025-06-27 22:59:46 +0100

    Add the functionality for scrolling the comments container into view once the user adds a new comment

--- blog/static/blog/js/add-comment.js
+++ blog/static/blog/js/add-comment.js
@@ -1,9 +1,15 @@
+const commentsContainer = document.querySelector(".comments-container");
+
 const commentToastAddEl = document.getElementById("comment-toast-add");
 const commentToastAdd = new bootstrap.Toast(commentToastAddEl);
 const newMessage = document.querySelector(".new-message");
 
 const displayToastAdd = function () {
   if (newMessage) {
+    commentsContainer.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
     commentToastAdd.show();
   }
 };
