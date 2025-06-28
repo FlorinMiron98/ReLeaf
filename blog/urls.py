d1a798548dac8d018244d8bff5d50a1b7fc1569e
Florin Miron <mironflorin123@yahoo.com>
2025-06-28 12:05:56 +0100

    Create a path for comment delete view

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.AllPostsView.as_view(), name='all-posts'),
     path('<slug:slug>', views.SinglePostView.as_view(), name='single-post'),
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update')
+    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete')
 ]
