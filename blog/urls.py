dcb3fc4bbc882f08ccadc92d6c6f40ecd5dcd975
Florin Miron <mironflorin123@yahoo.com>
2025-06-28 19:13:59 +0100

    Remove the slug from the paths

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.AllPostsView.as_view(), name='all-posts'),
     path('<slug:slug>', views.SinglePostView.as_view(), name='single-post'),
-    path('<slug:slug>/comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
-    path('<slug:slug>/comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete')
+    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete')
 ]
